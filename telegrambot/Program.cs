b87d79764f7d403157ee23b5ae9be66cc891a13e
Artur <veldergard@gmail.com>
2021-06-29 17:49:50 +0300

    started db connection

--- telegrambot/Program.cs
+++ telegrambot/Program.cs
@@ -1,55 +1,141 @@
 ﻿using System;
+using System.IO;
 using Telegram.Bot;
-using Telegram.Bot.Args;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace telegramBot
+namespace TelegramBot
 {
     class Program
     {
-        // private static TelegramBotClient client;
-        //
-        // static void Main(string[] args)
-        // {
-        //     client = new TelegramBotClient("1839671942:AAFruLDYynRsOvIlpEnf4YIDsTAqdEok2CI");
-        //     client.OnMessage += BotOnMessageReceived;
-        //     client.OnMessageEdited += BotOnMessageReceived;
-        //     client.StartReceiving();
-        //     Console.ReadLine();
-        //     client.StopReceiving(); 
-        //     Console.WriteLine("Hello World!");
-        // }
-        //
-        // private async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
-        // {
-        //     var message = messageEventArgs.Message;         
-        //     if (message?.Type == MessageType.TextMessage)
-        //     {
-        //         await client.SendTextMessageAsync(message.Chat.Id, message.Text);
-        //     }
-        // }
-        
-        private static ITelegramBotClient _botClient;
-
         static void Main() {
-            _botClient = new TelegramBotClient("1839671942:AAFruLDYynRsOvIlpEnf4YIDsTAqdEok2CI");
-            _botClient.OnMessage += Bot_OnMessage;
-            _botClient.StartReceiving();
-            Console.ReadKey();
-            _botClient.StopReceiving();
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            IServiceCollection services = new ServiceCollection();
+            ConfigureIoC(services, configuration);
+            var serviceProvider = services.BuildServiceProvider();
+            services.AddSingleton<IServiceProvider>(serviceProvider);
+            var handler = serviceProvider.GetService<ProjectModerationBotService>()!;
+            try
+            {
+                handler.Handle();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
-        static async void Bot_OnMessage(object sender, MessageEventArgs e) {
-            if (e.Message.Text != null)
-            {
-                Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+        private static void ConfigureIoC(IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("1839671942:AAFruLDYynRsOvIlpEnf4YIDsTAqdEok2CI"));
+            services.AddSingleton(configuration);
+            
+            var connection = configuration.GetConnectionString("RoundDB");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(connection));
 
-                await _botClient.SendTextMessageAsync(
-                    chatId: e.Message.Chat,
-                    text:   "You said:\n" + e.Message.Text
-                );
+            if (configuration.GetValue<bool>("UseAzureStorage"))
+            {
+                services.AddScoped(_ => new Func<string, IFileStorage>(container =>
+                    new AzureFileStorage(configuration, container)));
+                services.AddScoped<ITokenProvider, AzureTokenProvider>();
+                
+                services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(@"server"));
             }
+            else
+            {
+                services.AddScoped(_ => new Func<string, IFileStorage>(_ =>
+                    new LocalDirectoryFileStorage(configuration)));
+                services.AddScoped<ITokenProvider, MockTokenProvider>();
+            }
+
+            services.AddSingleton<IFileStoragePath, FileStoragePath>();
+            services.AddSingleton<IMediaFormatter, ImageMagickFormatter>();
+            services.AddSingleton<ImagePreviewHandler>();
         }
     }
 }
+
+// using System;
+// using System.Configuration;
+// using System.IO;
+// using System.Threading.Tasks;
+// using Krujok.Online.Core.Interfaces;
+// using Krujok.Online.Core.Services;
+// using Krujok.Online.Data;
+// using Krujok.Online.FileStorage.Azure.Services;
+// using Krujok.Online.FileStorage.Interfaces;
+// using Krujok.Online.FileStorage.LocalDirectory.Services;
+// using Krujok.Online.Media.Interfaces;
+// using Krujok.Online.Media.Services;
+// using Microsoft.AspNetCore.DataProtection;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.DependencyInjection;
+// using log4net;
+// using Microsoft.Extensions.Logging;
+//
+// namespace Krujok.Tools.MediaPreviewBuilder
+// {
+//     public static class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             var configuration = new ConfigurationBuilder()
+//                 .AddJsonFile("appsettings.json")
+//                 .Build();
+//             
+//             IServiceCollection services = new ServiceCollection();
+//
+//             ConfigureIoC(services, configuration);
+//
+//             var serviceProvider = services.BuildServiceProvider();
+//
+//             services.AddSingleton<IServiceProvider>(serviceProvider);
+//
+//             var handler = serviceProvider.GetService<ImagePreviewHandler>()!;
+//
+//             var logger = LogManager.GetLogger(typeof(Program));
+//
+//             try
+//             {
+//                 handler.Handle();
+//             }
+//             catch (Exception e)
+//             {
+//                 logger.Error(e);
+//             }
+//         }
+//
+//         private static void ConfigureIoC(IServiceCollection services, IConfiguration configuration)
+//         {
+//             services.AddLogging(builder => builder.AddLog4Net("log4net.config"));
+//             services.AddSingleton(configuration);
+//             
+//             var connection = configuration.GetConnectionString("DefaultConnection");
+//             services.AddDbContext<ApplicationDbContext>(options =>
+//                 options.UseNpgsql(connection));
+//
+//             if (configuration.GetValue<bool>("UseAzureStorage"))
+//             {
+//                 services.AddScoped(_ => new Func<string, IFileStorage>(container =>
+//                     new AzureFileStorage(configuration, container)));
+//                 services.AddScoped<ITokenProvider, AzureTokenProvider>();
+//                 
+//                 services.AddDataProtection().PersistKeysToFileSystem(new DirectoryInfo(@"server"));
+//             }
+//             else
+//             {
+//                 services.AddScoped(_ => new Func<string, IFileStorage>(_ =>
+//                     new LocalDirectoryFileStorage(configuration)));
+//                 services.AddScoped<ITokenProvider, MockTokenProvider>();
+//             }
+//
+//             services.AddSingleton<IFileStoragePath, FileStoragePath>();
+//             services.AddSingleton<IMediaFormatter, ImageMagickFormatter>();
+//             services.AddSingleton<ImagePreviewHandler>();
+//         }
+//     }
+// }
---
+++ telegrambot/ProjectModerationBotService.cs
@@ -0,0 +1,18 @@
+﻿using Telegram.Bot;
+
+namespace TelegramBot
+{
+    public class ProjectModerationBotService
+    {
+        private readonly ITelegramBotClient _botClient;
+
+        public ProjectModerationBotService(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+        
+        public void Start()
+        {
+        }
+    }
+}
